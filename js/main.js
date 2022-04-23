26e3d8b27368ef85e2382920fbce8fb90f32a7e7
Tee O <tdopress@gmail.com>
2022-04-23 12:37:21 -0400

    added art wall carousel

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ let arrOfArtworks = null;
 document.querySelectorAll('.department').forEach((el) => el.addEventListener('click', setDeparmentId))
 document.querySelector('#fetch').addEventListener('click', fetchArtworks)
 document.querySelectorAll('.wall').forEach((el) => el.addEventListener('click', hideImgThumbnails))
+
+document.querySelector('.right-arrow').addEventListener('click', moveCarouselRight)
+document.querySelector('.left-arrow').addEventListener('click', moveCarouselLeft)
                   
 function setDeparmentId() {
     departmentId = departmentKeys[this.innerText]
@@ -56,25 +59,50 @@ function fetchArtworks() {
     })
     .catch(err => {console.log(err)})
 }
+
+
 // Takes in an array of artworks and generates on screen each item
 function showArtworks(arr) {
 
     // Reset count
-    if(count > (MAX_ARTWORKS -1)) count = 0
+    if(count > (MAX_ARTWORKS - 1)) count = 0
+    else if(count < 0) count = MAX_ARTWORKS - 1
+
 
     fetch(`${arr[count].api_link}?fields=title,image_id,thumbnail,department_title,artist_display`)
     .then(res => res.json())
     .then(item => {
         console.log(item)
-        document.querySelector('h2').innerText = item.data.title;
-        document.querySelector('img').src = `https://www.artic.edu/iiif/2/${item.data.image_id}/full/400,/0/default.jpg`
-        document.querySelector('img').alt = item.data.thumbnail.alt_text
+
+        // Unhide elements
+        let elements = document.querySelectorAll('.wall-art')
+
+    
+
+        elements.forEach((el, i) => {
+            el.classList.remove('hidden')
+        
+        })
+
+        //Display art details
+        document.querySelector('.art-title').innerText = item.data.title;
+        document.querySelector('.art-pic').src = `https://www.artic.edu/iiif/2/${item.data.image_id}/full/400,/0/default.jpg`
+        document.querySelector('.art-pic').alt = item.data.thumbnail.alt_text
         document.querySelector('#artist').innerText = item.data.artist_display;
 
         
 
     })
     .catch(err => {console.log(err)})
+}
+
+function moveCarouselLeft() {
+    count--; 
+    showArtworks(arrOfArtworks);
+}
+function moveCarouselRight() { 
+    count++; 
+    showArtworks(arrOfArtworks);
 }
 
 function hideImgThumbnails() {
@@ -85,4 +113,6 @@ function hideImgThumbnails() {
     }
     imgs = document.querySelector('.center-wall').classList.add('fill-screen')
 
+    
+    showArtworks(arrOfArtworks);
 }
